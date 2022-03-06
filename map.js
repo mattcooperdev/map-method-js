25385f280ad72f43672c6e5704d428a9b8e2e8c0
YesCoops <coopandmgc@gmail.com>
2022-03-06 12:35:26 +0000

    Practice map method

--- map.js
+++ map.js
@@ -5,9 +5,21 @@
  */
 
 // Using a for loop
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+for (let num of nums) {
+  results.push(num * 2);
+}
+
+console.log(results);
 
 
 // Using map()
+// const multByTwo = function (num) {
+//   return num * 2;
+// }
+const mapResults = nums.map(num => num * 2);
+console.log(mapResults);
 
 
 // Simplified w/ map()
@@ -37,3 +49,6 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentsWithIds = students.map(student => [student.name, student.id]);
+console.log(studentsWithIds);
